0e4d58b3f7228c077baf4df7bb8ab7c211191e15
prasetyoow <praztamer@gmail.com>
2023-04-15 15:13:28 +0700

    first init

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+function ChannelDetail() {
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+
+  const { id } = useParams();
+
+  console.log(videos);
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part="snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+
+    fetchFromAPI(`search?channelId=${id}&part="snippet&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
+
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            background:
+              "radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%);",
+          }}
+        />
+      </Box>
+    </Box>
+  );
+}
+
+export default ChannelDetail;

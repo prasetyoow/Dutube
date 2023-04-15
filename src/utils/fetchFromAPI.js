969591e3142c81f801026ddd18d48c436bc04476
prasetyoow <praztamer@gmail.com>
2023-04-15 15:13:10 +0700

    added fetch from api

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    // "X-RapidAPI-Key": "9ca1afc3e9msh6be3dc4542ed486p1ef9d6jsn53d8ed3e3de3",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
